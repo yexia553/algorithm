ef208070631ff3771bd7893362eb73c8c5773fe5
zhixiang-pan <zhixiang_pan@trendmicro.com>
2021-08-25 20:08:20 +0800

    完善算法验证

--- sorting/selected_sorting.py
+++ sorting/selected_sorting.py
@@ -3,22 +3,53 @@
     每次扫描一遍所有的数据，找到最小的值跟第一数字交换位置，
     然后从第二个开始再扫描一次，找到最小的值然后跟第二个数字交换位置，
     重复以上步骤
+
+TODO:
+优化思路：在每一次循环的时候同时找出最小值和最大值，这样可以减少一半的循环
 """
+import random
+
 class SelectedSorting():
-    def __init__(self) -> None:
-        self.unsorted_list = [1, 5, 8, 3, 9, 10, 30, 23, 7, 34]
+    def __init__(self):
+        pass
     
+    def generate_list(self):
+        """
+        生成一个随机的列表
+        """
+        l = list()
+        for _ in range(50):
+            l.append(random.randint(1, 10000))
+        
+        return l
+
     def selected_sorting(self):
-        for i in range(len(self.unsorted_list)):
+        """
+        排序
+        """
+        unsorted_list = self.generate_list()
+        for i in range(len(unsorted_list)):
             min_idx = i
-            for j in range(i+1, len(self.unsorted_list)):
-                if self.unsorted_list[j] < self.unsorted_list[min_idx]:
+            for j in range(i+1, len(unsorted_list)):
+                # 执行次数：n*(n-1)/2,所以时间复杂度是：O(n^2)
+                if unsorted_list[j] < unsorted_list[min_idx]:
                     min_idx = j
-            tmp = self.unsorted_list[i]
-            self.unsorted_list[i] = self.unsorted_list[min_idx]
-            self.unsorted_list[min_idx] = tmp
-            print("循环的次数：%s, 当前数组的值：%s" % (i, self.unsorted_list))
+            # 以下三行中通过临时变量替换原列表中的值，
+            # 没有新增任何列表，整个排序过程中只有最开始的那一个列表，
+            # 所以空间复杂度是O(1)
+            tmp = unsorted_list[i]
+            unsorted_list[i] = unsorted_list[min_idx]
+            unsorted_list[min_idx] = tmp
+            # print("循环的次数：%s, 当前数组的值：%s" % (i, unsorted_list))
+        print(unsorted_list)
+
+    def validation(self):
+        """
+        验证排序算法
+        """
+        for _ in range(50):
+            self.selected_sorting()
 
 
 sorting = SelectedSorting()
-sorting.selected_sorting()
+sorting.validation()
