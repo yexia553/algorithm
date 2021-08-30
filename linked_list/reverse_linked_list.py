f14440da1b49e6e8994faf318071790d640561b9
zhixiang-pan <zhixiang_pan@trendmicro.com>
2021-08-30 22:02:33 +0800

    添加链表的笔记

--- linked_list/reverse_linked_list.py
+++ linked_list/reverse_linked_list.py
@@ -4,10 +4,10 @@
 """
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
